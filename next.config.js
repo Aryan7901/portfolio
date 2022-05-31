ef210d8fec53e73683abc3ec513f6d80c8edd2a4
Aryan7901 <85547188+Aryan7901@users.noreply.github.com>
2022-05-31 06:34:49 +0530

    added js config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 const withPreact = require("next-plugin-preact");
 
 module.exports = withPreact({
-  webpack: (config, { dev }) => {
-    // Replace React with Preact only in client production build
-    if (!dev) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-        "react/jsx-runtime": "preact/jsx-runtime",
-      });
-    }
-
-    return config;
-  },
   experimental: {
     modern: true,
   },
